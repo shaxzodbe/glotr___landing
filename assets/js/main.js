2e7b37e3a9b838b500ef09728b08e9631e065e6a
Shaxzodbek22 <sulaymanovshaxzod@gmail.com>
2022-03-17 13:41:33 +0500

    all 2

--- assets/js/main.js
+++ assets/js/main.js
@@ -175,5 +175,5 @@ const sr = ScrollReveal({
 sr.reveal(`.home__img, .new__container, .footer__container`)
 sr.reveal(`.home__data`, {delay: 300})
 sr.reveal(`.giving__content, .gift__card`,{interval: 70})
-sr.reveal(`.celebrate__data, .message__form, .footer__img1`,{origin: 'left'})
+sr.reveal(`.celebrate__data, .message__form, .accordion__container, .footer__img1`,{origin: 'left'})
 sr.reveal(`.celebrate__img, .message__img, .footer__img2`,{origin: 'right'})
